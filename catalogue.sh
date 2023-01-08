58c7e0f48307dc9d13540d1437c9f910b1b49a45
LokeshV <lokeshviswa428@gmail.com>
2023-01-08 21:25:18 +0530

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,22 @@
 script_location=$(pwd)
-set -e
-
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
 yum install nodejs -y
-
-#useradd roboshop
+useradd roboshop
 mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 rm -rf /app/*
 cd /app
-
 unzip /tmp/catalogue.zip
 cd /app
 npm install
 
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
-cp ${script}
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
 mongo --host mongodb-dev.lokeshviswa44.online </app/schema/catalogue.js
