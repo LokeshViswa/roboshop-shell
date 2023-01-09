dcb4208f11d87fa7d8e9eca774ecbdad87e7deb6
LokeshV <lokeshviswa428@gmail.com>
2023-01-09 13:40:12 +0530

    Code Dry

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,74 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir -p /app
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[1;32mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo "Refer Log file for more information, LOG - ${LOG}"
+    exit 1
+  fi
+}
+
+echo -e "\e[35m Configuring NodeJS repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{LOG}
+status_check
+
+echo -e "\e[35m Install NodeJS\e[0m"
+yum install nodejs -y &>>{LOG}
+status_check
+
+echo -e "\e[35m Add Aplication User\e[0m"
+useradd roboshop &>>{LOG}
+status_check
+
+mkdir -p /app &>>{LOG}
+
+
+echo -e "\e[35m Downloading App Content\e[0m"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{LOG}
+status_check
+
+echo -e "\e[35m Cleanup old Content\e[0m"
+rm -rf /app/* &>>{LOG}
+status_check
+
+echo -e "\e[35m Extracting App Content\e[0m"
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
+unzip /tmp/catalogue.zip &>>{LOG}
+status_check
 
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+echo -e "\e[35m Installing NodeJS Dependencies\e[0m"
+cd /app &>>{LOG}
+npm install &>>{LOG}
+status_check
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+echo -e "\e[35m Configuring Catalogue Services\e[0m"
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>{LOG}
+status_check
 
-mongo --host mongodb-dev.lokeshviswa44.online </app/schema/catalogue.js
+echo -e "\e[35m Reload SystemD\e[0m"
+systemctl daemon-reload &>>{LOG}
+status_check
+
+echo -e "\e[35m Enable Catalogue Services\e[0m"
+systemctl enable catalogue &>>{LOG}
+status_check
+
+echo -e "\e[35m Start Catalogue Services\e[0m"
+systemctl start catalogue &>>{LOG}
+status_check
+
+echo -e "\e[35m Configuring Mongo Repo\e[0m"
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>{LOG}
+status_check
+
+echo -e "\e[35m Install Mongo Client\e[0m"
+yum install mongodb-org-shell -y &>>{LOG}
+status_check
+
+echo -e "\e[35m Load Schema\e[0m"
+mongo --host mongodb-dev.lokeshviswa44.online </app/schema/catalogue.js &>>{LOG}
+status_check
 
