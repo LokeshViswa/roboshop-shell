0c404201528d5e917c644ad15f00376692af7c20
LokeshV <lokeshviswa428@gmail.com>
2024-04-10 12:34:56 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -2,7 +2,7 @@ script_location=${pwd}
 LOG=/tmp/roboshop.log
 
 status_check() {
-  if [$? -eq 0 ]; then
+  if [ $? -eq 0 ]; then
     echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[1;31mFAILURE\e[0m"
